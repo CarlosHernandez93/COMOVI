dbd8a5c43aa64cd33dfde8c66503fdeb292c68ee
CarlosHernandez93 <64928869+CarlosHernandez93@users.noreply.github.com>
2023-03-24 10:51:25 -0500

    Creacion del NavBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,17 @@
+import React from "react";
+import LogoUSC from "../assets/LogoUSC.png"
+
+function NavBar(){
+    return(
+        <header className="bg-[#172072] flex items-center px-5 py-2.5">
+            <img className="h-[100px] mr-3.5" src={LogoUSC} alt="Logo USC"></img>
+            <h1 className="text-white text-2xl font-semibold">CONVOCATORIA DE MONITORIA VIRTUAL</h1>
+            <div className="grow text-right">
+                <a className="text-[#172072] bg-white mr-7   p-2 rounded hover:bg-[#dcdcdc] font-medium" href="#">Iniciar Sesion</a>
+                <a className="text-[#172072] bg-white p-2 rounded hover:bg-[#dcdcdc] font-medium" href="#">Registrar</a>
+            </div>
+        </header>
+    );
+}
+
+export default NavBar;
