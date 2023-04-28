4b444c5c8c4a13aa9bc12c48040ac9a6f1699f3d
sebacho102002 <juan.bejarano03@usc.edu.co>
2023-04-27 23:41:14 -0500

    Styles

---
+++ client/src/Pages/LoginPage.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import LogoUSC from "../assets/LogoUSC2.png"
+
+function LoginPage(){
+    return(
+        <div className="h-screen flex bg-fondo-azul1 bg-cover justify-center items-center">
+            <div className="h-[85%] w-[35%] bg-white rounded-md flex flex-col items-center py-5">
+                <img src={LogoUSC} alt="LogoUSC" className="w-56 h-56 mb-9"/>
+                <input type="tex" placeholder="Nombre de usuario" className=" border-[1px] border-neutral-500 rounded p-[.5em] w-[80%] mb-5"/>
+                <input type="password" placeholder="Contraseña" className=" border-[1px] border-neutral-500 rounded p-[.5em] w-[80%] mb-5"/>
+                <button className="w-[80%] bg-[#172072] text-white font-medium p-[.5em] rounded">Iniciar Sesion</button>
+            </div>
+        </div>
+    );
+}
+
+export default LoginPage;
